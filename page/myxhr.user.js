09b4f9e00da153fe4e1940aacb094c317d693c7b
lilium <liliumbookair@gmail.com>
2022-09-04 01:28:10 +0900

    work in progress...

---
+++ page/myxhr.user.js
@@ -0,0 +1,42 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        http://*/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  // Your code here...
+  var Original = window.XMLHttpRequest;
+  var MyXMLHttpRequest = function (...args) {
+    var mocked = new Original(...args);
+    mocked.onload = () => {
+      console.log(res);
+      var res = mocked.response;
+      var ctype = mocked.getResponseHeader("content-type");
+      console.log(ctype);
+
+      if (ctype.includes("json")) {
+        var jsn = JSON.parse(res);
+        console.log("# json");
+        console.log(jsn);
+      }
+
+      if (ctype.includes("text/html")) {
+        var text = res.slice(1, 200);
+        console.log("# text");
+        console.log(args);
+        console.log(text);
+      }
+    };
+    return mocked;
+  };
+
+  window.XMLHttpRequest = MyXMLHttpRequest;
+})();
