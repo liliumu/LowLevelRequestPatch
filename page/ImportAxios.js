c807d5286d6885c59c5aeb85c5aaaf52f86bd690
lilium <liliumbookair@gmail.com>
2022-09-04 02:15:48 +0900

    fmt

--- page/ImportAxios.js
+++ page/ImportAxios.js
@@ -1,4 +1,4 @@
-var src = "https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"
-var script = document.createElement('script')
-script.src = src
-document.head.appendChild(script)
+var src = "https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js";
+var script = document.createElement("script");
+script.src = src;
+document.head.appendChild(script);
