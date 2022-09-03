de0a4f23895f27590171001029e7875409c336c6
lilium <liliumbookair@gmail.com>
2022-09-04 04:12:02 +0900

    Add XHRLog_TweetDeck.user.js

---
+++ page/userjs/XHRLog_TweetDeck.user.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name         XHRLog_TweetDeck
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://tweetdeck.twitter.com/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=twitter.com
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  // Your code here...
+  var OriginalXMLHttpRequest = window.XMLHttpRequest;
+
+  var MyXMLHttpRequest = function (...args) {
+    var mocked = new OriginalXMLHttpRequest(...args);
+
+    setTimeout(function () {
+      var xhr = mocked;
+      window.xhr = xhr;
+      console.log("");
+      console.log(mocked);
+      console.log("url  <-", xhr.responseURL);
+      // console.log('head <-', xhr.getAllResponseHeaders().replace('\r', '').replace('\n', ' '))
+      console.log(
+        "body <-",
+        xhr.responseText.slice(0, 400).replace("\r", "").replace("\n", " ")
+      );
+    }, 500);
+
+    return mocked;
+  };
+
+  window.XMLHttpRequest = MyXMLHttpRequest;
+})();
