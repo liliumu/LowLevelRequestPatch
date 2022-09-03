793242b6f46aefeb1883663b565636b2dab84548
lilium <liliumbookair@gmail.com>
2022-09-04 01:39:12 +0900

    working

--- page/myxhr.user.js
+++ page/myxhr.user.js
@@ -15,28 +15,29 @@
   // Your code here...
   var Original = window.XMLHttpRequest;
   var MyXMLHttpRequest = function (...args) {
-      var mocked = new Original(...args);
-      mocked.onload = () => {
-          console.log(res);
-          var res = mocked.response;
-          var ctype = mocked.getResponseHeader("content-type");
-          console.log(ctype);
-
-          if (ctype.includes("json")) {
-              var jsn = JSON.parse(res);
-              console.log("# json");
-              console.log(jsn);
-          }
-
-          if (ctype.includes("text/html")) {
-              var text = res.slice(0, 200);
-              console.log("# text");
-              console.log(args);
-              console.log(text);
-          }
-      };
-      return mocked;
+    var mocked = new Original(...args);
+    mocked.onload = () => {
+      console.log(res);
+      var res = mocked.response;
+      var ctype = mocked.getResponseHeader("content-type");
+      console.log(ctype);
+  
+      if (ctype.includes("json")) {
+        MOCKED = mocked;
+        var jsn = JSON.parse(res);
+        console.log("# json");
+        console.log(jsn);
+      }
+  
+      if (ctype.includes("text/html")) {
+        var text = res.slice(1, 200);
+        console.log("# text");
+        console.log(args);
+        console.log(text);
+      }
+    };
+    return mocked;
   };
-
+  
   window.XMLHttpRequest = MyXMLHttpRequest;
 })();
