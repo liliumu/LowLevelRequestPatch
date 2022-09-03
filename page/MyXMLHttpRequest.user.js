cc3d879104477751a8c055abe6e0f1c3aac0d93b
lilium <liliumbookair@gmail.com>
2022-09-04 03:09:01 +0900

    update

--- page/MyXMLHttpRequest.user.js
+++ page/MyXMLHttpRequest.user.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        http://*/*
+// @match        https://none/*/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
